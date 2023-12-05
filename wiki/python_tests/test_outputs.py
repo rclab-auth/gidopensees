7f0f764b47b6142edb60896ff1e6c50025300785
Konstantinos Mixios <k.mixios@gmail.com>
2023-12-05 12:35:00 +0200

    Tested Plane Truss

--- wiki/python_tests/test_outputs.py
+++ wiki/python_tests/test_outputs.py
@@ -83,5 +83,18 @@ class FileComparison(unittest.TestCase):
             print(f"{common_file}: {filecmp.cmp(py_file, tcl_file)}")
         self.assertTrue(filecmp.cmp(py_file, tcl_file))
 
+    def test_Plane_Truss(self):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        python_outputs = os.path.join(current_directory, "datasets", "Plane Truss",
+                                      "Python outputs")
+        tcl_outputs = os.path.join(current_directory, "datasets", "Plane Truss",
+                                   "Tcl outputs")
+        d = filecmp.dircmp(python_outputs, tcl_outputs)
+        for common_file in d.common_files:
+            py_file = f"{python_outputs}/{common_file}"
+            tcl_file = f"{tcl_outputs}/{common_file}"
+            print(f"{common_file}: {filecmp.cmp(py_file, tcl_file)}")
+        self.assertTrue(filecmp.cmp(py_file, tcl_file))
+
 if __name__ == '__main__':
     unittest.main()
