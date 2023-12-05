31df09ebd4108eb95cf475d6c38b0ded0290f0d6
Konstantinos Mixios <k.mixios@gmail.com>
2023-12-05 13:29:55 +0200

    Tested Retaining Wall

--- wiki/python_tests/test_outputs.py
+++ wiki/python_tests/test_outputs.py
@@ -90,11 +90,40 @@ class FileComparison(unittest.TestCase):
         tcl_outputs = os.path.join(current_directory, "datasets", "Plane Truss",
                                    "Tcl outputs")
         d = filecmp.dircmp(python_outputs, tcl_outputs)
+        num_files = len(d.common_files)
+        count = 0   
         for common_file in d.common_files:
             py_file = f"{python_outputs}/{common_file}"
             tcl_file = f"{tcl_outputs}/{common_file}"
             print(f"{common_file}: {filecmp.cmp(py_file, tcl_file)}")
-        self.assertTrue(filecmp.cmp(py_file, tcl_file))
+            count += 1
+            if filecmp.cmp(py_file, tcl_file):
+                count += 1
+        if count == num_files:
+            self.assertTrue(False)
+        else:
+            self.assertTrue(True)
+
+    def test_Retaining_Wall(self):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        python_outputs = os.path.join(current_directory, "datasets", "Retaining Wall",
+                                      "Python outputs")
+        tcl_outputs = os.path.join(current_directory, "datasets", "Retaining Wall",
+                                   "Tcl outputs")
+        d = filecmp.dircmp(python_outputs, tcl_outputs)
+        num_files = len(d.common_files)
+        count = 0
+        for common_file in d.common_files:
+            py_file = f"{python_outputs}/{common_file}"
+            tcl_file = f"{tcl_outputs}/{common_file}"
+            print(f"{common_file}: {filecmp.cmp(py_file, tcl_file)}")
+            count += 1
+            if filecmp.cmp(py_file, tcl_file):
+                count += 1
+        if count == num_files:
+            self.assertTrue(False)
+        else:
+            self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
